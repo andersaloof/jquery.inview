0dfddd0cce80faa55e3940b5190f9d9f77efdb7b
andersaloof <anders@aloof.no>
2013-11-11 11:19:36 +0100

    Fixed calculations, minified and added example

--- jquery.inview.js
+++ jquery.inview.js
@@ -2,23 +2,26 @@
  * author Anders S. Oefsdahl
  *    - based on the idea of Remy Sharp, http://remysharp.com/2009/01/26/element-in-view-event-plugin/
  *    - forked from http://github.com/protonet/jquery.inview
+ *    - visible area calculation from https://github.com/Webmie/jquery.inview
  */
-(function ($) {
+(function($) {
   var inviewObjects = {}, viewportSize, viewportOffset,
-      d = document, w = window, documentElement = d.documentElement, expando = $.expando;
+          d = document, w = window, documentElement = d.documentElement, expando = $.expando;
 
   $.event.special.inview = {
     add: function(data) {
-      inviewObjects[data.guid + "-" + this[expando]] = { data: data, $element: $(this) };
+      inviewObjects[data.guid + "-" + this[expando]] = {data: data, $element: $(this)};
     },
-
     remove: function(data) {
-      try { delete inviewObjects[data.guid + "-" + this[expando]]; } catch(e) {}
+      try {
+        delete inviewObjects[data.guid + "-" + this[expando]];
+      } catch (e) {
+      }
     }
   };
 
   function getViewportSize() {
-    var mode, domObject, size = { height: w.innerHeight, width: w.innerWidth };
+    var mode, domObject, size = {height: w.innerHeight, width: w.innerWidth};
 
     // if this is correct then return it. iPad has compat Mode, so will
     // go into check clientHeight/clientWidth (which has the wrong value).
@@ -26,11 +29,11 @@
       mode = d.compatMode;
       if (mode || !$.support.boxModel) { // IE, Gecko
         domObject = mode === 'CSS1Compat' ?
-          documentElement : // Standards
-          d.body; // Quirks
+                documentElement : // Standards
+                d.body; // Quirks
         size = {
           height: domObject.clientHeight,
-          width:  domObject.clientWidth
+          width: domObject.clientWidth
         };
       }
     }
@@ -40,8 +43,8 @@
 
   function getViewportOffset() {
     return {
-      top:  w.pageYOffset || documentElement.scrollTop   || d.body.scrollTop,
-      left: w.pageXOffset || documentElement.scrollLeft  || d.body.scrollLeft
+      top: w.pageYOffset || documentElement.scrollTop || d.body.scrollTop,
+      left: w.pageXOffset || documentElement.scrollLeft || d.body.scrollLeft
     };
   }
 
@@ -49,30 +52,30 @@
     var $elements = $(), elementsLength, i = 0;
 
     $.each(inviewObjects, function(i, inviewObject) {
-      var selector  = inviewObject.data.selector,
-          $element  = inviewObject.$element;
+      var selector = inviewObject.data.selector,
+              $element = inviewObject.$element;
       $elements = $elements.add(selector ? $element.find(selector) : $element);
     });
 
     elementsLength = $elements.length;
     if (elementsLength) {
-      viewportSize   = viewportSize   || getViewportSize();
+      viewportSize = viewportSize || getViewportSize();
       viewportOffset = viewportOffset || getViewportOffset();
 
-      for (; i<elementsLength; i++) {
+      for (; i < elementsLength; i++) {
         // Ignore elements that are not in the DOM tree
         if (!$.contains(documentElement, $elements[i])) {
           continue;
         }
 
-        var $element      = $($elements[i]),
-            elementSize   = { height: $element.height(), width: $element.width() },
-            elementOffset = $element.offset(),
-            inView        = $element.data('inview'),
-            visiblePartX,
-            visiblePartY,
-            visiblePartsMerged;
-    
+        var $element = $($elements[i]),
+                elementSize = {height: $element.height(), width: $element.width()},
+        elementOffset = $element.offset(),
+                inView = $element.data('inview'),
+                visiblePartX,
+                visiblePartY,
+                visiblePartsMerged;
+
         // Don't ask me why because I haven't figured out yet:
         // viewportOffset and viewportSize are sometimes suddenly null in Firefox 5.
         // Even though it sounds weird:
@@ -81,46 +84,63 @@
         if (!viewportOffset || !viewportSize) {
           return;
         }
-        
-        if($element.data('inview-percentage-y') || $element.data('inview-percentage-x')){
-          var percentX = $element.data('inview-percentage-x') ? parseInt($element.data('inview-percentage-x'), 10)/100 : 0;
-          var percentY = $element.data('inview-percentage-y') ? parseInt($element.data('inview-percentage-y'), 10)/100 : 0;
-          if (elementOffset.top + elementSize.height > viewportOffset.top &&
-              elementOffset.top < viewportOffset.top + viewportSize.height &&
-              elementOffset.left + elementSize.width > viewportOffset.left &&
-              elementOffset.left < viewportOffset.left + viewportSize.width) {
-            var visibleSizeX = (viewportSize.width - Math.abs(elementOffset.left - viewportOffset.left)) / elementSize.width;
-            var visibleSizeY = (viewportSize.height - Math.abs(elementOffset.top - viewportOffset.top)) / elementSize.height;
+
+        if (elementOffset.top + elementSize.height > viewportOffset.top &&
+                elementOffset.top < viewportOffset.top + viewportSize.height &&
+                elementOffset.left + elementSize.width > viewportOffset.left &&
+                elementOffset.left < viewportOffset.left + viewportSize.width) {
+          visiblePartX = (viewportOffset.left > elementOffset.left ?
+                  'right' : (viewportOffset.left + viewportSize.width) < (elementOffset.left + elementSize.width) ?
+                  'left' : 'both');
+          visiblePartY = (viewportOffset.top > elementOffset.top ?
+                  'bottom' : (viewportOffset.top + viewportSize.height) < (elementOffset.top + elementSize.height) ?
+                  'top' : 'both');
+          visiblePartsMerged = visiblePartX + "-" + visiblePartY;
+
+          // Check if visible part of element fulfills required horizontal/vertical percentages
+          if ($element.data('inview-percentage-x') || $element.data('inview-percentage-y')) {
+            var percentX = parseInt($element.data('inview-percentage-x') || 0, 10) / 100;
+            var percentY = parseInt($element.data('inview-percentage-y') || 0, 10) / 100;
             
-            if(visibleSizeX >= percentX && visibleSizeY >= percentY){
-              visiblePartsMerged = percentX + "-" + percentY;
+            var visibleWidth = Math.max(0,
+                    Math.min(
+                            elementOffset.left + elementSize.width,
+                            viewportOffset.left + viewportSize.width
+                            )
+                    - Math.max(
+                            elementOffset.left,
+                            viewportOffset.left
+                            )
+                    );
+            var visiblePercentageX = visibleWidth / elementSize.width;
+            
+            var visibleHeight = Math.max(0,
+                    Math.min(
+                            elementOffset.top + elementSize.height,
+                            viewportOffset.top + viewportSize.height
+                            )
+                    - Math.max(
+                            elementOffset.top,
+                            viewportOffset.top
+                            )
+                    );
+            var visiblePercentageY = visibleHeight / elementSize.height;
+            console.log($element.attr('id'), visibleWidth, visibleHeight);
+            
+            if (visiblePercentageX >= percentX && visiblePercentageY >= percentY) {
               if (!inView || inView !== visiblePartsMerged) {
-                $element.data('inview', visiblePartsMerged).trigger('inview', [true, percentX, percentY]);
+                $element.data('inview', visiblePartsMerged).trigger('inview', [true, visiblePartX, visiblePartY]);
               }
-            } else if (inView){
+            } else if (inView) {
               $element.data('inview', false).trigger('inview', [false]);
             }
-          } else if (inView) {
-            $element.data('inview', false).trigger('inview', [false]);
-          }
-        }else{
-          if (elementOffset.top + elementSize.height > viewportOffset.top &&
-              elementOffset.top < viewportOffset.top + viewportSize.height &&
-              elementOffset.left + elementSize.width > viewportOffset.left &&
-              elementOffset.left < viewportOffset.left + viewportSize.width) {
-            visiblePartX = (viewportOffset.left > elementOffset.left ?
-              'right' : (viewportOffset.left + viewportSize.width) < (elementOffset.left + elementSize.width) ?
-              'left' : 'both');
-            visiblePartY = (viewportOffset.top > elementOffset.top ?
-              'bottom' : (viewportOffset.top + viewportSize.height) < (elementOffset.top + elementSize.height) ?
-              'top' : 'both');
-            visiblePartsMerged = visiblePartX + "-" + visiblePartY;
+          } else {
             if (!inView || inView !== visiblePartsMerged) {
               $element.data('inview', visiblePartsMerged).trigger('inview', [true, visiblePartX, visiblePartY]);
             }
-          } else if (inView) {
-            $element.data('inview', false).trigger('inview', [false]);
           }
+        } else if (inView) {
+          $element.data('inview', false).trigger('inview', [false]);
         }
       }
     }
@@ -129,7 +149,7 @@
   $(w).bind("scroll resize", function() {
     viewportSize = viewportOffset = null;
   });
-  
+
   // IE < 9 scrolls to focused elements without firing the "scroll" event
   if (!documentElement.addEventListener && documentElement.attachEvent) {
     documentElement.attachEvent("onfocusin", function() {
